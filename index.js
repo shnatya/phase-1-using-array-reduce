cd787539c21de0f5c3d8f6643a2bedc6fed36731
Anastasia Shatokhina <shnatya@gmail.com>
2021-08-24 13:22:29 -0700

    commit tree 89bb7a7914a0a62ba15de273cdaa4a4e4838910a

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
 // Code your solution here
+//const reducer = (accumulator, item) => accumulator += item;
+let totalBatteries = batteryBatches.reduce(((accumulator, item) => accumulator += item), 0);
